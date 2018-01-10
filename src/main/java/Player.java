e5625495a5a1ab7752d891e1e3c85bfc96ff23b8
AN JAEJIN <e175731@ie.u-ryukyu.ac.jp>
2018-01-10 20:36:48 +0900

    2-2. put one player(y座標が2桁以上の場合、表示されるエラーを修正)

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -8,17 +8,39 @@ public class Player {
         char[][] factor;
         factor = board.SetBoard();
 
-        char c_x = s . charAt ( 0 );
-        char c_y = s . charAt ( 2 );
-        int x;
-        int y;
-
-        x = (int)c_x;
-        x -= 64;
-        y = (int)c_y;
-        int z = (int)'0';
-        y -= z;
-        factor[y][x] = 'O';
+        if (s.length()==3){
+            int x;
+            int y;
+            char c_x = s . charAt ( 0 );
+            char c_y = s . charAt ( 2 );
+            x = (int)c_x;
+            x -= 64;
+            y = (int)c_y;
+            int z = (int)'0';
+            y -= z;
+            factor[y][x] = 'O';
+        }else{
+            if(s.length()==4){
+                int x;
+                int y;
+                int y1;
+                int y2;
+                char c_x = s . charAt ( 0 );
+                char c_y1 = s . charAt ( 2 );
+                char c_y2 = s . charAt ( 3 );
+                x = (int)c_x;
+                x -= 64;
+                y1 = (int)c_y1;
+                y2 = (int)c_y2;
+                int z = (int)'0';
+                y1 -= z;
+                y2 -= z;
+                y = 10*y1 + y2;
+                factor[y][x] = 'O';
+            }else{
+                System.out.println("Please enter location again:");
+            }
+        }
 
         int i;
         int j;
