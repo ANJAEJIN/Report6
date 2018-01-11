a33e7a872a505bc2cf64614a7bf338028fa109fd
AN JAEJIN <e175731@ie.u-ryukyu.ac.jp>
2018-01-11 23:14:36 +0900

    3-3. 4つの方向についての最終的な終了判定を追加

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -18,19 +18,61 @@ public class Player {
     }
 
     public boolean finish(Board b, Location l, char c){
-        int point=1;
+        int point1=1;
+        int point2=1;
+        int point3=1;
+        int point4=1;
         int i=l.x;
         int j=l.y;
+        /*horizontal*/
         while (b.factor[j][i+1]==c && i<15){
             i +=1;
-            point +=1;
+            point1 +=1;
         }
         i = l.x;
         while (b.factor[j][i-1]==c && i>1){
-            point +=1;
+            point1 +=1;
             i -=1;
         }
-        if (point == 5){
+        /*Vertical*/
+        while (b.factor[j+1][i]==c && j<15){
+            j +=1;
+            point2 +=1;
+        }
+        j = l.y;
+        while (b.factor[j-1][i]==c && j>1){
+            point2 +=1;
+            j -=1;
+        }
+        /*diagonal1*/
+        while (b.factor[j+1][i+1]==c && j<15 && i<15){
+            i +=1;
+            j +=1;
+            point3 +=1;
+        }
+        i = l.x;
+        j = l.y;
+        while (b.factor[j-1][i-1]==c && j>1 && i>1){
+            point3 +=1;
+            i -=1;
+            j -=1;
+        }
+        /*diagonal2*/
+        while (b.factor[j+1][i-1]==c && j<15 && i>1){
+            i -=1;
+            j +=1;
+            point4 +=1;
+        }
+        i = l.x;
+        j = l.y;
+        while (b.factor[j-1][i+1]==c && j>1 && i<15){
+            point4 +=1;
+            i +=1;
+            j -=1;
+        }
+
+        if (point1 == 5 || point2 == 5 || point3 == 5 || point4 == 5){
+            System.out.println("winner is"+c+". Congratulations");
             return false;
         }else {
             return true;
